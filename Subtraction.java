17cbf9401d5fa70e946b4f28fe0247ad65aa0fc8
NimishaChavan <chavannaman480@gmail.com>
2025-05-14 09:53:25 +0530

    subb

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int num1 = 15;  // First number
+        int num2 = 7;   // Second number
+
+        int difference = num1 - num2;  // Subtraction
+
+        System.out.println("The difference is: " + difference);
+    }
+}
